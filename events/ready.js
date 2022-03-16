8545262550f19012befd62092c04249dae0b3506
Alexander Vorobyev <alex@nder.pro>
2022-03-16 10:42:13 +0300

    🦀 Presence

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,7 @@ module.exports = {
 	name: "ready",
 	once: true,
 	execute(client) {
+		client.user.setPresence({ activities: [{ type: "WATCHING", name: "your every move🦀" }] })
 		console.log(`Ready! Logged in as ${client.user.tag}`)
 	},
 }

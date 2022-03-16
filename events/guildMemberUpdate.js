899943892ae311d2631e450310a0fe4452a6d65d
Alexander Vorobyev <alex@nder.pro>
2022-03-16 11:18:22 +0300

    📡 Role Change Notifications

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,26 +1,55 @@
+const { categoryId, roleId, botChannelId } = require("../config.json")
+
 module.exports = {
 	name: "guildMemberUpdate",
-	execute(oldMember, newMember) {
-		console.log(oldMember)
+	async execute(oldMember, newMember) {
+		await newMember.guild.channels.fetch()
+		await newMember.guild.roles.fetch()
 
-		console.log(newMember)
+		let category = newMember.guild.channels.cache.get(categoryId)
 
-		let category = newMember.guild.channels.cache.get("953453092500819969")
+		let role = newMember.guild.roles.cache.get(roleId)
+
+		let botChannel = newMember.guild.channels.cache.get(botChannelId)
 
 		let userName = newMember.user.username.toLowerCase()
 
-		if (newMember._roles.includes("670868788337573908")) {
-			category.createChannel(userName)
+		if (newMember._roles.includes(roleId)) {
+			let newChannel = await category.createChannel(userName)
 
-			console.log("Added trial role")
+			let outputEmbed = {
+				color: role.hexColor,
+				title: `New ${role.name} member detected: ${
+					newMember.nickname ? `${newMember.nickname}` : `${newMember.user.username}`
+				}`,
+				description: `Created a review channel: <#${newChannel.id}>`,
+				thumbnail: {
+					url: newMember.user.displayAvatarURL(),
+				},
+			}
+
+			await botChannel.send({ embeds: [outputEmbed] })
 		} else {
-			category.children.forEach(async (channel) => {
-				if (channel.name.includes(userName)) {
-					await channel.delete()
-				}
-			})
+			await Promise.all(
+				category.children.map(async (channel) => {
+					if (channel.name.includes(userName)) {
+						await channel.delete()
+					}
+				})
+			)
 
-			console.log("Removed trial role")
+			let outputEmbed = {
+				color: role.hexColor,
+				title: `${newMember.nickname ? `${newMember.nickname}` : `${newMember.user.username}`} is no longer ${
+					role.name
+				}!`,
+				description: `Deleting assoiciated review channel.`,
+				thumbnail: {
+					url: newMember.user.displayAvatarURL(),
+				},
+			}
+
+			await botChannel.send({ embeds: [outputEmbed] })
 		}
 	},
 }
